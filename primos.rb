9f804fe6f94c3b9a3706cb228296b6d228f45383
Hector <hector12j@gmail.com>
2015-04-06 15:44:42 -0430

    Listo el reto mas dificil.. los primos

---
+++ primos.rb
@@ -0,0 +1,21 @@
+def main
+	if 1 % 2 !=0
+ 	 	puts "el numero 1 es primo"
+	end
+	if 2 % 2 !=0
+ 	 	puts "el numero 2 es primo"
+	end
+	if 3 % 2 !=0
+ 	 	puts "el numero 3 es primo"
+	end
+	if 4 % 2 !=0
+ 	 	puts "el numero 4 es primo"
+	end
+	if 5 % 2 !=0
+ 	 	puts "el numero 5 es primo"
+	end
+	if 6 % 2 !=0
+ 	 	puts "el numero 6 es primo"
+	end
+end
+main
