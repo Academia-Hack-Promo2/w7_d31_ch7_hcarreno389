ec0884d3d5c422a25528974d277764537c315389
Hector <hector12j@gmail.com>
2015-04-07 09:56:56 -0430

    listo primos

--- primos.rb
+++ primos.rb
@@ -1,20 +1,18 @@
 def main
-	if 1 % 2 !=0
- 	 	puts "el numero 1 es primo"
+	
+	if 2 % 2 == 0 && 2 % 1 == 0 
+  	 	puts "el numero 2 es primo"
 	end
-	if 2 % 2 !=0
- 	 	puts "el numero 2 es primo"
-	end
-	if 3 % 2 !=0
+	if 3 % 1 == 0 && 3 % 3 == 0 && 3 % 2 != 0
  	 	puts "el numero 3 es primo"
 	end
-	if 4 % 2 !=0
+	if 4 % 1 == 0 && 4 % 4 == 0 && 4 % 3 != 0 && 4 % 2 != 0
  	 	puts "el numero 4 es primo"
 	end
-	if 5 % 2 !=0
+	if 5 % 1 == 0 && 5 % 5 == 0 && 5 % 3 != 0
  	 	puts "el numero 5 es primo"
 	end
-	if 6 % 2 !=0
+	if 6 % 1 ==0 && 6 % 6 == 0 && 6 % 3 != 0
  	 	puts "el numero 6 es primo"
 	end
 end
